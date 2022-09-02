cb5e51352109fa94d34dc4f58f1d47838ad278b5
Malou <marie-louise.zesiger@edu.gbsl.ch>
2022-09-02 16:50:00 +0200

    Füge Änderungen der Lektion hinzu

---
+++ scripts/ex1.py
@@ -0,0 +1,14 @@
+ALPHABET = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+MESSAGE = [12, 9, 19, 20, 0, 18, 9, 4, 4, 12, 5]
+
+for i in MESSAGE:
+    print(ALPHABET[i], end='')
+# for 9 in MESSAGE
+#     print(ALPHABET['I'], end= '')
+#     print(ALPHABET)
+
+print(ALPHABET[12], end='')
+print(ALPHABET[9], end='')
+print(ALPHABET[19], end='')
+print(ALPHABET[20], end='')
+print(ALPHABET[0], end='')
